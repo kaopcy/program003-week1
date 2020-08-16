7ae20bf18945c53791190e1b684e8be23e19445d
PIYACHAI KAEWCHUM <63010604@KMITL.AC.TH>
2020-08-16 14:13:29 +0700

    fix if x<0

--- Project3/Source.cpp
+++ Project3/Source.cpp
@@ -4,14 +4,23 @@ int main()
     int i=0,o=0,sum = 0,x;
     printf("insert number of row and colum : ");
     scanf_s("%d",& x);
-    for (int i = 0; i <= x; i++)
+    if (x < 0)
     {
-        for (int o = 0; o < i; o++)
-        {
-            printf("* ");
-        }
-        sum += i;
-        printf("\n");
+        printf("Error");
     }
-    printf("\nSum of all * is %d", sum);
+    else
+    {
+        for (int i = 0; i <= x; i++)
+        {
+            for (int o = 0; o < i; o++)
+            {
+                printf("* ");
+            }
+            sum += i;
+            printf("\n");
+        }
+        printf("\nSum of all * is %d", sum);
+
+    }
+    
 }
